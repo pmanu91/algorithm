d7e9b6da1441d934ff1c379aac79e158d18204e7
pmanu91 <qkraksdn4@naver.com>
2018-09-08 11:17:58 +0900

    course1 완료

---
+++ Course2/src/C2_InOut.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class C2_InOut {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int n = scan.nextInt();
+		
+		int[][] matrix = new int[10][10];
+		
+		for (int m=0; m<n; m++) {
+			// cross point change
+			if (matrix[m][m] == 0) 
+				matrix[m][m] = 1;
+			else 
+				matrix[m][m] = 0;
+			
+			// row change
+			for (int i=0; i<10; i++) {
+				for (int j=0; j<10; j++) {
+					if (i == m) {
+						if (j == m) {
+							continue;
+						} else {
+							if (matrix[i][j] == 0)
+								matrix[i][j] = 1;
+							else
+								matrix[i][j] = 0;
+						}
+					}
+					
+				}
+			}
+			
+			// col change
+			for (int i=0; i<10; i++) {
+				for (int j=0; j<10; j++) {
+					if (j == m) {
+						if (i == m) {
+							continue;
+						} else {
+							if (matrix[i][j] == 0)
+								matrix[i][j] = 1;
+							else
+								matrix[i][j] = 0;
+						}
+					}
+				}
+			}
+		
+		}
+		
+		for (int i=0; i<10; i++) {
+			for (int j=0; j<10; j++) {
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
